329673d2ebc38a1e597cb183493206f41dc816c8
Stefan Eskelid <stefanes@duck.com>
2022-11-12 15:17:42 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # cloudflared
+
+Connect a local Cloudflare tunnel to you network in a few easy steps.
+
+# 1. Setup your domain
+
+Setup your own domain name (e.g. example.com) and use the Clodflare nameservers. If you do not have one, you can get one for free at [Freenom](https://www.freenom.com/en/index.html?lang=en) following e.g. [this article](https://www.linkedin.com/pulse/what-do-domain-name-how-get-one-free-tobias-brenner?trk=public_post-content_share-article).
+
+# 2. Download cloudflared
+
+Download and extract `cloudflared` [from Cloudflare](https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide/local/#1-download-and-install-cloudflared).
+
+# 3. Run the script to connect your tunnel
+
+To connect a tunnel routing the default set of services (see [here](https://github.com/stefanes/cloudflared/blob/main/connect-tunnel.ps1#L6-L14)):
+
+```powershell
+& .\cloudflared\connect-tunnel.ps1 -CloudflaredPath 'C:\path\to\cloudflared.exe' -HostName 'myhostname.tk'
+```
+
+To route other services, provide the `-AdditionalServices` parameter:
+
+```powershell
+& .\cloudflared\connect-tunnel.ps1 -CloudflaredPath 'C:\path\to\cloudflared.exe' -HostName 'myhostname.tk' -Service 'http://192.168.1.100:8080' -AdditionalServices @(
+    @{
+        domain  = 'www'
+        service = 'http://192.168.1.100:8080'
+    }
+    @{
+        domain  = 'something'
+        service = 'http://192.168.1.254'
+    }
+)
+```
---
+++ connect-tunnel.ps1
@@ -0,0 +1,97 @@
+[CmdletBinding()]
+param (
+  [Alias('Path')]
+  [string] $CloudflaredPath = "$PSScriptRoot\cloudflared-windows-amd64.exe",
+  
+  [Parameter(Mandatory = $true)]
+  [Alias('Host')]
+  [string] $HostName,
+
+  [string] $Service = 'http://homeassistant.local:8123',
+
+  [Object] $AdditionalServices = @(
+    @{
+      domain  = 'status'
+      service = 'http://homeassistant.local:4357'
+    }
+    @{
+      domain  = 'router'
+      service = 'http://192.168.1.1'
+    }
+  ),
+
+  [Alias('Tunnel')]
+  [string] $TunnelName = ($HostName -replace '[^a-z0-9]', '-')
+)
+
+Write-Host "Logging in to Cloudflare..." -ForegroundColor Green
+Write-Host "Note: Choose any website that you have added into your account. The authentication is account-wide and you can use the same authentication flow for multiple hostnames in your account regardless of which you choose in this step."
+if (-Not (Test-Path -Path "$env:USERPROFILE\.cloudflared\cert.pem")) {
+  & $CloudflaredPath tunnel login
+}
+else {
+  Write-Host "Already logged in. To force a re-login, please remove '$env:USERPROFILE\.cloudflared\cert.pem'."
+}
+
+Write-Host "Creating tunnel..." -ForegroundColor Green
+& $CloudflaredPath tunnel list | Tee-Object -Variable tunnelList | Out-Null
+$tunnelUuid = $null
+foreach ($line in $tunnelList) {
+  if ($line -match '^(?<uuid>[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})\s+(?<name>\S+)') {
+    if ($Matches['name'] -eq $tunnelName) {
+      Write-Host "Tunnel already exists."
+      $tunnelUuid = $Matches['uuid']
+    }
+  }
+}
+if (-Not $tunnelUuid) {
+  # Create new tunnel 
+  & $CloudflaredPath tunnel create $TunnelName
+  & $CloudflaredPath tunnel list | Tee-Object -Variable tunnelList | Out-Null
+  foreach ($line in $tunnelList) {
+    if ($line -match '^(?<uuid>[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})\s+(?<name>\S+)') {
+      if ($Matches['name'] -eq $tunnelName) {
+        $tunnelUuid = $Matches['uuid']
+      }
+    }
+  }
+}
+$tunnelList | Out-Host
+
+Write-Host "Creating tunnel config..." -ForegroundColor Green
+$config = @"
+tunnel: $tunnelUuid
+credentials-file: $env:USERPROFILE\.cloudflared\$tunnelUuid.json
+logfile: $env:USERPROFILE\.cloudflared\$tunnelUuid.log
+ingress:
+  - hostname: $HostName
+    service: $Service
+    originRequest:
+      noTLSVerify: true
+"@
+# Additional services
+foreach ($additionalService in $AdditionalServices) {
+  $config += @"
+
+  - hostname: $($additionalService.domain).$HostName
+    service: $($additionalService.service)
+    originRequest:
+      noTLSVerify: true
+"@
+}
+# Catch all service 
+$config += @"
+
+  - service: http_status:404
+"@
+$config | Out-File -FilePath "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml"
+Get-Content -Path "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml"
+
+Write-Host "Creating DNS entries..." -ForegroundColor Green
+& $CloudflaredPath tunnel route dns -f $tunnelUuid "$HostName"
+foreach ($additionalService in $AdditionalServices) {
+  & $CloudflaredPath tunnel route dns -f $tunnelUuid "$($additionalService.domain).$HostName"
+}
+
+Write-Host "Connecting tunnel..." -ForegroundColor Green
+& $CloudflaredPath --no-autoupdate tunnel --metrics="0.0.0.0:36500" --config "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml" run "$tunnelUuid"
