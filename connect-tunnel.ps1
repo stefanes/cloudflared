656f9a630b3a3317e6c7f55fb9073142e5b86de6
Stefan Eskelid <stefanes@duck.com>
2022-11-14 00:29:38 +0100

    Fix for running multiple tunnels on the same host

--- connect-tunnel.ps1
+++ connect-tunnel.ps1
@@ -27,16 +27,17 @@ param (
 )
 
 Write-Host "Logging in to Cloudflare..." -ForegroundColor Green
-Write-Host "Note: Choose any website that you have added into your account. The authentication is account-wide and you can use the same authentication flow for multiple hostnames in your account regardless of which you choose in this step." -ForegroundColor DarkGray
-if (-Not (Test-Path -Path "$env:USERPROFILE\.cloudflared\cert.pem")) {
+if (-Not (Test-Path -Path "$env:USERPROFILE\.cloudflared\$TunnelName.pem")) {
   & $CloudflaredPath tunnel login
+  Rename-Item -Path "$env:USERPROFILE\.cloudflared\cert.pem" -NewName "$TunnelName.pem"
 }
 else {
-  Write-Host "Already logged in. To force a re-login, please remove '$env:USERPROFILE\.cloudflared\cert.pem'."
+  Write-Host "Already logged in. To force a re-login, please remove '$env:USERPROFILE\.cloudflared\$TunnelName.pem'."
 }
+$cert ="--origincert=`"$env:USERPROFILE\.cloudflared\$TunnelName.pem`""
 
 Write-Host "Creating tunnel..." -ForegroundColor Green
-& $CloudflaredPath tunnel list | Tee-Object -Variable tunnelList | Out-Null
+& $CloudflaredPath $cert tunnel list | Tee-Object -Variable tunnelList | Out-Null
 $tunnelUuid = $null
 foreach ($line in $tunnelList) {
   if ($line -match '^(?<uuid>[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})\s+(?<name>\S+)') {
@@ -48,8 +49,8 @@ foreach ($line in $tunnelList) {
 }
 if (-Not $tunnelUuid) {
   # Create new tunnel 
-  & $CloudflaredPath tunnel create $TunnelName
-  & $CloudflaredPath tunnel list | Tee-Object -Variable tunnelList | Out-Null
+  & $CloudflaredPath $cert --cred-file="$env:USERPROFILE\.cloudflared\$TunnelName.json" tunnel create $TunnelName
+  & $CloudflaredPath $cert tunnel list | Tee-Object -Variable tunnelList | Out-Null
   foreach ($line in $tunnelList) {
     if ($line -match '^(?<uuid>[0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})\s+(?<name>\S+)') {
       if ($Matches['name'] -eq $tunnelName) {
@@ -63,8 +64,8 @@ Write-Host ($tunnelList | Out-String) -ForegroundColor DarkGray
 Write-Host "Creating tunnel config..." -ForegroundColor Green
 $config = @"
 tunnel: $tunnelUuid
-credentials-file: $env:USERPROFILE\.cloudflared\$tunnelUuid.json
-logfile: $env:USERPROFILE\.cloudflared\$tunnelUuid.log
+credentials-file: $env:USERPROFILE\.cloudflared\$TunnelName.json
+logfile: $env:USERPROFILE\.cloudflared\$TunnelName.log
 ingress:
   - hostname: $HostName
     service: $Service
@@ -96,17 +97,17 @@ $config += @"
 
   - service: http_status:404
 "@
-$config | Out-File -FilePath "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml"
-Get-Content -Path "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml"
+$config | Out-File -FilePath "$env:USERPROFILE\.cloudflared\$TunnelName.yml"
+Get-Content -Path "$env:USERPROFILE\.cloudflared\$TunnelName.yml"
 
 Write-Host "Creating DNS entries..." -ForegroundColor Green
-& $CloudflaredPath tunnel route dns -f $tunnelUuid "$HostName"
+& $CloudflaredPath $cert tunnel route dns -f $tunnelUuid "$HostName"
 foreach ($additionalService in $DefaultServices) {
-  & $CloudflaredPath tunnel route dns -f $tunnelUuid "$($additionalService.domain).$HostName"
+  & $CloudflaredPath $cert tunnel route dns -f $tunnelUuid "$($additionalService.domain).$HostName"
 }
 foreach ($additionalService in $AdditionalServices) {
-  & $CloudflaredPath tunnel route dns -f $tunnelUuid "$($additionalService.domain).$HostName"
+  & $CloudflaredPath $cert tunnel route dns -f $tunnelUuid "$($additionalService.domain).$HostName"
 }
 
 Write-Host "Connecting tunnel..." -ForegroundColor Green
-& $CloudflaredPath --no-autoupdate tunnel --metrics="0.0.0.0:36500" --config "$env:USERPROFILE\.cloudflared\$tunnelUuid.yml" run "$tunnelUuid"
+& $CloudflaredPath $cert --no-autoupdate tunnel --config "$env:USERPROFILE\.cloudflared\$TunnelName.yml" run "$tunnelUuid"
